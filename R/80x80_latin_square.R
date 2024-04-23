a4f551f63650ca442a97ddaf70dc3fb77c77cd3c
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-04-23 08:47:20 +0000

    Move main plotting scripts.

---
+++ R/80x80_latin_square.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+library(here)
+library(keedwell)
+
+source(here("R", "first-row-natural.R"))
+
+l_order <- 80
+
+L <- first_row_natural(l_order) |>
+  add_rows(2:l_order)
+
+ggplot(L, aes(column, row)) +
+  geom_tile(aes(fill = symbol)) +
+  scale_y_reverse() +
+  coord_fixed() +
+  theme_void() +
+  theme(
+    legend.position  = "none",
+  ) +
+  scale_fill_viridis_c(option = "magma")
+
+ggsave(file = here("plots", "80x80_latin-square.png"), width = 8, height = 8)
