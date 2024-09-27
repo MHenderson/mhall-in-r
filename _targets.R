db9d99e0fb9984fae88d5d8a986558e4a0613ae8
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-09-27 09:09:29 +0000

    Add new targets for examples of order 12, 13, 14 and 16.

--- _targets.R
+++ _targets.R
@@ -4,10 +4,28 @@ tar_option_set(
   packages = c("ggplot2", "here", "keedwell", "scico", "tibble")
 )
 
+tar_source()
+
 list(
+  tar_target(
+       name = latin_square_12,
+    command = first_row_natural(12) |> add_rows(2:12)
+  ),
+  tar_target(
+       name = latin_square_13,
+    command = first_row_natural(13) |> add_rows(2:13)
+  ),
+  tar_target(
+       name = latin_square_14,
+    command = first_row_natural(14) |> add_rows(2:14)
+  ),
   tar_target(
        name = latin_square_15,
     command = first_row_natural(15) |> add_rows(2:15)
+  ),
+  tar_target(
+       name = latin_square_16,
+    command = first_row_natural(16) |> add_rows(2:16)
   ),
   tar_target(
        name = latin_square_80,
@@ -18,16 +36,24 @@ list(
     command = first_row_natural(495) |> add_rows(2:495)
   ),
   tar_target(
-    name = plot_latin_square_15,
-    command = {
-      ggplot(latin_square_15, aes(column, row)) +
-        geom_tile(aes(fill = symbol)) +
-        geom_text(aes(label = symbol), size = 5, colour = "white") +
-        scale_y_reverse() +
-        coord_fixed() +
-        theme_void() +
-        theme(legend.position  = "none")
-    }
+       name = plot_latin_square_12,
+    command = plot_latin_square(latin_square_12) + geom_text(aes(label = symbol), size = 5, colour = "white")
+  ),
+  tar_target(
+       name = plot_latin_square_13,
+    command = plot_latin_square(latin_square_13)
+  ),
+  tar_target(
+       name = plot_latin_square_14,
+    command = plot_latin_square(latin_square_14)
+  ),
+  tar_target(
+       name = plot_latin_square_15,
+    command = plot_latin_square(latin_square_15)
+  ),
+  tar_target(
+       name = plot_latin_square_16,
+    command = plot_latin_square(latin_square_16)
   ),
   tar_target(
     name = plot_latin_square_80,
@@ -54,10 +80,46 @@ list(
     }
   ),
   tar_target(
+    name = save_plot_12,
+    command = ggsave(
+          plot = plot_latin_square_12,
+      filename = here("plots", "12x12_latin-square.png"),
+         width = 8,
+        height = 8
+    )
+  ),
+  tar_target(
+    name = save_plot_13,
+    command = ggsave(
+          plot = plot_latin_square_13,
+      filename = here("plots", "13x13_latin-square.png"),
+         width = 8,
+        height = 8
+    )
+  ),
+  tar_target(
+    name = save_plot_14,
+    command = ggsave(
+          plot = plot_latin_square_14,
+      filename = here("plots", "14x14_latin-square.png"),
+         width = 8,
+        height = 8
+    )
+  ),
+  tar_target(
     name = save_plot_15,
     command = ggsave(
           plot = plot_latin_square_15,
       filename = here("plots", "15x15_latin-square.png"),
+         width = 8,
+        height = 8
+    )
+  ),
+  tar_target(
+    name = save_plot_16,
+    command = ggsave(
+          plot = plot_latin_square_16,
+      filename = here("plots", "16x16_latin-square.png"),
          width = 8,
         height = 8
     )
